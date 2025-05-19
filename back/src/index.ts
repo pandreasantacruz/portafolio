5608233b136680a899891c289e8ba0ef9a19acb0
pandreasantacruz <andreasantacruz123@gmail.com>
2025-05-19 15:17:05 -0500

    "Servidor"

--- back/src/index.ts
+++ back/src/index.ts
@@ -3,20 +3,14 @@ import express from "express";
 import dotenv from "dotenv";
 import { AppDataSource } from "./database/data.source";
 import contactRoutes from "./routes/contact.routes";
-
-dotenv.config();
-
-const app = express();
-app.use(express.json());
-app.use("/api", contactRoutes);
-
-const PORT = process.env.PORT || 3001;
+import { PORT } from "./config/envs";
+import server from "./server";
 
 AppDataSource.initialize()
   .then(() => {
-    console.log("📦 DB conectada");
-    app.listen(PORT, () => {
-      console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+    console.log(" DB conectada");
+    server.listen(PORT, () => {
+      console.log(` Servidor corriendo en http://localhost:${PORT}`);
     });
   })
   .catch((err) => console.error("Error al conectar DB:", err));
