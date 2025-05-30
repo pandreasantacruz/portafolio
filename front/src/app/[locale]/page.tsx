4829c32817cf7f367ea96f27c3e4624a3f252ed4
pandreasantacruz <andreasantacruz123@gmail.com>
2025-05-29 20:24:15 -0500

    "Traduccion lista"

--- front/src/app/[locale]/page.tsx
+++ front/src/app/[locale]/page.tsx
@@ -1,12 +1,16 @@
+import AboutMe from "./aboutMe/page";
 import { useTranslations } from "next-intl";
 import { Link } from "@/i18n/navigation";
-
-export default function HomePage() {
+export default function Home() {
   const t = useTranslations("HomePage");
+
   return (
     <div>
-      <h1>{t("title")}</h1>
-      <Link href="/about">{t("about")}</Link>
+      <AboutMe></AboutMe>
+      <div>
+        <h1>{t("title")}</h1>
+        <Link href="/about">{t("about")}</Link>
+      </div>
     </div>
   );
 }
