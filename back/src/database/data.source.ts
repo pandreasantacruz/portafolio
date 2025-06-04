f48858a9cac899484118d815c2a55485bc495e8b
pandreasantacruz <andreasantacruz123@gmail.com>
2025-06-04 18:24:41 -0500

    "RENDER OTRA VEZ"

--- back/src/database/data.source.ts
+++ back/src/database/data.source.ts
@@ -1,11 +1,8 @@
 import { DataSource } from "typeorm";
-import { HOST, PORT_PG } from "../config/envs";
 import { Contact } from "../entities/Contact";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: HOST,
-  port: Number(PORT_PG),
   url: process.env.DATABASE_URL,
   synchronize: true,
   entities: [Contact],
