bb374ba30174252856ce7b376b3005f2cbb634bb
Paula Andrea Santacruz <137830226+pandreasantacruz@users.noreply.github.com>
2025-06-04 20:35:46 -0500

    Update envs.ts

--- back/src/config/envs.ts
+++ back/src/config/envs.ts
@@ -7,11 +7,3 @@ export const PORT_PG = process.env.DB_PORT;
 export const USERNAME = process.env.DB_USERNAME;
 export const PASSWORD = process.env.DB_PASSWORD;
 export const DATABASE = process.env.DB_NAME;
-console.log("ENV DEBUG:", {
-  HOST,
-  PORT_PG,
-  USERNAME,
-  PASSWORD: typeof PASSWORD,
-  DATABASE,
-  PORT,
-=======
