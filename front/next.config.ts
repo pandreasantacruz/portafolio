39e96b7ccab702e6794be933f4e9af044202db31
Paula Andrea Santacruz <137830226+pandreasantacruz@users.noreply.github.com>
2025-06-04 21:28:08 -0500

    Update next.config.ts

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin({  localePath: "./messages"
+});
 const nextConfig: NextConfig = {};
 
 export default withNextIntl(nextConfig);
